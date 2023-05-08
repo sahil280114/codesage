615f82481e58c0cbcb1e31aad688c4eef092fa45
sahil280114 <sahil280114@gmail.com>
2023-05-08 16:42:00 +0200

    install koala

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `!function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_95d4dd307465c0824381217a348a499aebce/sdk.js"),(document.body || document.head).appendChild(n)}();`,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
